ebe4d2e2de529964ba6175f98972f45d22f2f851
Dzaky <dzaky.badawi@gmail.com>
2023-09-10 15:38:08 +0700

    chore: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'react/react-in-jsx-scope': 0,
+        'import/no-extraneous-dependencies': 0,
+        ' @typescript-eslint/no-non-null-assertion': 0,
+    },
+};
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+    return <div className="bg-red-300 px-2 text-red-200 py-5">App</div>;
+}
+
+export default App;
